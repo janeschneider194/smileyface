5d7980203363894b5da4b8dc96405da8b008e8d9
janeschneider194 <42978529+janeschneider194@users.noreply.github.com>
2018-09-12 14:06:56 -0400

    Add files via upload

---
+++ FirstSubmission.java
@@ -0,0 +1,20 @@
+
+/**
+ * 
+ * @author jane schneider
+ * smiley face
+ *
+ */
+public class FirstSubmission
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("\\\\\\\\\\\\");
+		System.out.println("| # # |");
+		System.out.println("|  #  |");
+		System.out.println("| \\_/ |");
+		System.out.println(" \\___/");
+	}
+	
+	}
